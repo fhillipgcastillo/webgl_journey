276648793188f4a7fe134beec9567c88657b32e7
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2022-09-16 00:12:48 -0400

    rotation and rotation orders

--- 05 - Transform Objects/src/script.js
+++ 05 - Transform Objects/src/script.js
@@ -11,7 +11,7 @@ const scene = new THREE.Scene()
  * Objects
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshStandardMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 mesh.position.set(1, -0.5, 0);
@@ -20,6 +20,31 @@ mesh.position.set(1, -0.5, 0);
 const strokeLenght = 1;
 const axesHelper = new THREE.AxesHelper(strokeLenght);
 scene.add(axesHelper);
+
+
+// scale
+mesh.scale.y = 0.5
+mesh.scale.x = 0.4
+// or
+mesh.scale.set(1, 1.5, 1);
+
+// rotation
+/*
+2 options
+* rotation
+ by mesh.rotation and receive an euler 
+ rotation will change all of the meshes axes from the prespective
+*/
+mesh.rotation.y = 0.3;
+// lets say we wamt tp halt rotate, so do this
+mesh.rotation.x = Math.PI;
+
+
+// to fix the rotation issues that the axes get changes, wee need to change
+// the rotation order
+mesh.rotation.reorder("YXZ");
+//then do the rotations
+mesh.rotation.y = 0.7;
 
 /**
  * Sizes
@@ -37,6 +62,13 @@ camera.position.z = 5
 camera.position.y = 1
 scene.add(camera)
 
+
+//temporatelly light to see the rotation
+const dirlight = new THREE.DirectionalLight("white", 1);
+dirlight.position.set(3, 5, 3);
+const ambientLight = new THREE.AmbientLight("blue", 0.1);
+
+scene.add(dirlight, ambientLight);
 /**
  * Renderer
  */

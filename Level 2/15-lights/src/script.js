e57d264ca5b7e9b5df2864a02a3858bd2e586a64
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2022-09-20 21:33:12 -0400

    Added ambient light

--- Level 2/15-lights/src/script.js
+++ Level 2/15-lights/src/script.js
@@ -20,12 +20,15 @@ const scene = new THREE.Scene()
  */
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+// const pointLight = new THREE.PointLight(0xffffff, 0.5)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
+// scene.add(pointLight)
+
+
 
 /**
  * Objects
--- Level 2/README.md
+++ Level 2/README.md
@@ -1,1 +1,10 @@
 # 15 - Light (#15)
+## Ambient light
+IT's applied omnidirectional lighting
+### Parameters
+1. color
+2. intensity
+
+```javascript
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+```

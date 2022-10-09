b612775b732460d9c7cfa2f8b5e5956f707f8e60
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2022-10-08 20:13:20 -0400

    added the z rotation value

--- Level 2/21-scroll-based-animation/src/script.js
+++ Level 2/21-scroll-based-animation/src/script.js
@@ -173,6 +173,7 @@ window.addEventListener("scroll", (e)=>{
                 ease: 'power2.inOut',
                 x: '+=6',
                 y: '+=3',
+                z: '+=1.5',
             }
         )
     }

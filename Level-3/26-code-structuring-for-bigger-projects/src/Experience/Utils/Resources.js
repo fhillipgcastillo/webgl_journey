4063b6953d2d2cd7fd30f927d78ad62e797dbbb1
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2023-07-04 00:01:06 -0400

    remove console logs

--- Level-3/26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
+++ Level-3/26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
@@ -28,17 +28,14 @@ export default class Resources extends EventEmitter{
             if(source.type === "gltfModel") {
                 this.loaders.gltfLoader.load(source.path, (file) => {
                     this.sourceLoaded(source, file);
-                    console.log(source, file);
                 })
             } else if(source.type === "texture") {
                 this.loaders.textureLoader.load(source.path, (file) => {
                     this.sourceLoaded(source, file);
-                    console.log(source, file);
                 })
             } else if(source.type === "cubeTexture") {
                 this.loaders.cubeTextureLoader.load(source.path, (file) => {
                     this.sourceLoaded(source, file);
-                    console.log(source, file);
                 })
             }
         }
